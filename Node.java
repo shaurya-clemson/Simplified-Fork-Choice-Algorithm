8a07930fbc1afb9945cd91d3d2bea6a43d65f32a
Shaurya Raghuvanshi <shaurya1096@gmail.com>
2024-01-01 00:43:25 -0600

    Added Node class

---
+++ Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    int number;
+    Node left, right;
+
+    public Node(int number) {
+        this.number = number;
+        this.left = null;
+        this.right = null;
+    }
+}
