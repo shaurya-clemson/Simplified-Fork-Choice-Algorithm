ac45891af3fbf47e4066fb93c0d4e28b03a472ab
Shaurya Raghuvanshi <shaurya1096@gmail.com>
2024-01-04 23:23:14 -0500

    Update Blockchain.java

--- Blockchain.java
+++ Blockchain.java
@@ -1,1 +1,14 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
 
+public class Blockchain {
+
+    private static Node buildTree(int depth) {
+        Node root = new Node(0);
+        addChildren(root, 0, depth);
+        return root;
+    }
